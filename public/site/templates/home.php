96eea4f88ef017871f3faf9d29eec436bc8ca8a4
Jesa Navarro <jesaa@me.com>
2017-05-20 05:57:15 -0500

    Add text auto admin

--- public/site/templates/home.php
+++ public/site/templates/home.php
@@ -2,11 +2,11 @@
   <!--  BKT Slides -->
 <?php $image=$page->img1;
         if($image)
-          $img_thumb= $image->height(400, array('quality' => 90, 'upscaling' => true, 'cropping' => false)); ?>
+          $img_thumb= $image->height(800, array('quality' => 90, 'upscaling' => true, 'cropping' => false)); ?>
   <div class="j-workspace slider" <?php if($image) { ?> style="background-image: url('<?php echo $img_thumb->url; ?>'); <?php } ?>">
     <div class="j-color-layer">
      <div class="j-wrap">
-       <h1>Diseño, implementación y operación de sistemas de <br> bicicletas públicas.</h1>
+       <h1 align="center"><?php echo $page->headline; ?></h1>
      </div>
     </div>
   </div>
@@ -15,21 +15,21 @@
    <section class="bkt-strengths">
      <div class="j-wrap grid">
       <div class="unit one-third">
-        <img src="<?php echo $config->urls->templates; ?>static/455375-1485995688/images/icono-diseño.svg" alt="Icono diseño fortalezas BKT bici pública">
+        <img src="<?php echo $config->urls->templates; ?>static/455375-1495244812/images/icono-diseño.svg" alt="Icono diseño fortalezas BKT bici pública">
         <h3>Diseño</h3>
         <p>
           Diagnóstico de la ciudad y definicón de polígonos de intervención, diseño de la red, dimensión y capacidad de estaciones dentro de un mapa.
         </p>
       </div>
       <div class="unit one-third">
-        <img src="<?php echo $config->urls->templates; ?>static/455375-1485995688/images/icono-implementacion.svg" alt="Icono implementación fortalezas BKT bici pública">
+        <img src="<?php echo $config->urls->templates; ?>static/455375-1495244812/images/icono-implementacion.svg" alt="Icono implementación fortalezas BKT bici pública">
         <h3>Implementación</h3>
         <p>
           Diagnóstico de la ciudad y definicón de polígonos de intervención, diseño de la red, dimensión y capacidad de estaciones dentro de un mapa.
         </p>
       </div>
       <div class="unit one-third">
-        <img src="<?php echo $config->urls->templates; ?>static/455375-1485995688/images/icono-operacion.svg" alt="Icono operación fortalezas BKT bici pública">
+        <img src="<?php echo $config->urls->templates; ?>static/455375-1495244812/images/icono-operacion.svg" alt="Icono operación fortalezas BKT bici pública">
         <h3>Operación</h3>
         <p>
           Diagnóstico de la ciudad y definicón de polígonos de intervención, diseño de la red, dimensión y capacidad de estaciones dentro de un mapa.
@@ -40,7 +40,7 @@
    <!--  BKT Mensaje Jumbo-->
    <section class="bkt-jumbo-message">
      <div class="j-wrap">
-       <p><strong>Transformamos la ciudad</strong> con el mejor equipo <br> humano y la mejor tecnología.</p>
+       <p><?php echo $page->txt1; ?></p>
        <hr>
      </div>
    </section>
@@ -48,13 +48,13 @@
    <section class="bkt-features j-bg-gray-lighter">
      <div class="bkt-features-wrap">
        <div class="bkt-features-wrap-text">
-         <h2>Experiencia</h2>
-         <p>Contamos con el equipo y las alianzas especializadas en Urbanismo, Análisis vial, Gestión de Proyectos, Área Jurídica, así como la relación con grupos ciudadanos interesados en cambiar el paradigma actual de ciudad.</p>
+         <h2><?php echo $page->title1; ?></h2>
+         <p><?php echo $page->txt2; ?></p>
          <a class="bkt-btn" href="">Saber más</a>
        </div>
        <?php $image=$page->img2;
         if($image)
-          $img_thumb= $image->size(600,400, array('quality' => 90, 'upscaling' => true, 'cropping' => false)); ?>
+          $img_thumb= $image->size(1200,800, array('quality' => 90, 'upscaling' => true, 'cropping' => false)); ?>
        <div class="bkt-features-wrap-photo" <?php if($image) { ?> style="background-image: url('<?php echo $img_thumb->url; ?>'); <?php } ?>">
        </div>
      </div>
@@ -65,18 +65,18 @@
        <!--  The next container show a photo in desktop and hidden in mobile for esthetic reason  -->
        <?php $image=$page->img3;
         if($image)
-          $img_thumb= $image->size(600,400, array('quality' => 90, 'upscaling' => true, 'cropping' => false)); ?>
+          $img_thumb= $image->size(1200,800, array('quality' => 90, 'upscaling' => true, 'cropping' => false)); ?>
        <div class="bkt-features-wrap-photo desktop" <?php if($image) { ?> style="background-image: url('<?php echo $img_thumb->url; ?>'); <?php } ?>">
        </div>
        <div class="bkt-features-wrap-text">
-         <h2>Asesoría</h2>
-         <p>Apoyamos con el diseño de criterios, geometrías e infraestructura para la movilidad no motorizada. La propuesta es muy simple: devolver la habitabilidad y flujos seguros a peatones y ciclistas, lo que demando un análisis en la zona, para dar solución principalmente a cruces y pasos a nivel.</p>
+         <h2><?php echo $page->title2; ?></h2>
+         <p><?php echo $page->txt3; ?></p>
          <hr class="right">
        </div>
        <!--  The next container show a photo in mobile and hidden in desktop for esthetic reason  -->
        <?php $image=$page->img3;
         if($image)
-          $img_thumb= $image->size(600,400, array('quality' => 90, 'upscaling' => true, 'cropping' => false)); ?>
+          $img_thumb= $image->size(1200,800, array('quality' => 90, 'upscaling' => true, 'cropping' => false)); ?>
        <div class="bkt-features-wrap-photo mobile" <?php if($image) { ?> style="background-image: url('<?php echo $img_thumb->url; ?>'); <?php } ?>">
        </div>
      </div>
@@ -86,18 +86,16 @@
    <section class="bkt-features j-bg-gray-regular" <?php if($image) { ?> style="background-image: url('<?php echo $image->url; ?>'); <?php } ?>">
      <div class="bkt-features-wrap">
        <div class="bkt-features-wrap-middle-down-text">
-        <h2>Transporte Masivo</h2>
+        <h2><?php echo $page->title3; ?></h2>
         <div class="text">
           <div class="subtitle-container">
             <p class="sub-title">
-              <strong>Transformamos la ciudad</strong>
-                con el mejor equipo humano
-                y la mejor tecnología.
+              <?php echo $page->txt4; ?>
             </p>
             <hr class="right">
           </div>
-          <p>Un Sistema de Bicicletas Públicas (SBP) es un servicio de transporte que tiene a la bicicleta como medio para generar desplazamientos de un punto a otro, recorridos de distancias </p>
-          <p>cortas recorridos de distancias cortas y funciona como un complemento en el último tramo de los trayectos de otros sistemas de transporte.</p>
+          <p><?php echo $page->txt5; ?></p>
+          <p><?php echo $page->txt6; ?></p>
         </div>
        </div>
      </div>
@@ -106,20 +104,12 @@
    <section class="bkt-features j-bg-color-warning">
      <div class="bkt-features-wrap">
        <div class="bkt-features-wrap-text fifthy-percent">
-         <h2>Por qué introducir el ciclismo urbano en las ciudades?</h2>
-         <ul>
-           <li>Servicio eficinete de transporte </li>
-           <li> Servico accesible y económico</li>
-           <li> Acción clara en pro de la salud de los ciudadanos</li>
-           <li> Acceptación ciudadana </li>
-           <li> Cohesión social</li>
-           <li> Implementación de un servicio en tan solo 6  meses </li>
-           <li> Alta aceptación ciudadana a bajo costo</li>
-         </ul>
+         <h2><?php echo $page->title4; ?></h2>
+         <?php echo $page->txt7; ?>
        </div>
        <?php $image=$page->img4;
         if($image)
-          $img_thumb= $image->size(600,400, array('quality' => 90, 'upscaling' => true, 'cropping' => false)); ?>
+          $img_thumb= $image->size(1200,800, array('quality' => 90, 'upscaling' => true, 'cropping' => false)); ?>
        <div class="bkt-features-wrap-photo fifthy-percent" <?php if($image) { ?> style="background-image: url('<?php echo $img_thumb->url; ?>'); <?php } ?>">
        </div>
      </div>
@@ -131,8 +121,8 @@
      <div class="blue-layer">
        <div class="j-wrap">
         <p>
-          <strong>¿por qué implementar un sistema de bicicletas públicas?</strong> <br>
-          Se implementan en un periodo corto de tiempo
+          <strong><?php echo $page->title5; ?></strong> <br>
+          S<?php echo $page->txt8; ?>
         </p>
         <hr>
       </div>
