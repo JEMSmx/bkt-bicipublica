fc16dc635a98c8f7f188e7692b73dd8f7fdca754
Jesa Navarro <jesaa@me.com>
2017-05-20 09:57:27 -0500

    Update experiencia template

--- public/site/templates/experiencia.php
+++ public/site/templates/experiencia.php
@@ -4,14 +4,13 @@
    <section class="bkt-features for-titles j-bg-gray-lighter">
       <div class="bkt-features-wrap for-titles">
         <div class="bkt-features-wrap-middle-down-title">
-          <h1>Experiencia</h1>
+          <h1><?php echo $page->title; ?></h1>
           <p class="sub-title">
-            un sistema de transporte público es capáz de transformar <br>
-            <strong>la ciudad y su agenda de movilidad</strong>
+            <?php echo str_replace("<p>", "", $page->txt1); ?>
           </p>
           <div class="paragraph">
-            <p>BKT bicipública S.A. de C.V. es una empresa ciudadana donde construimos nuestros proyectos desde acciones locales ligadas a los ciudadanos. Estamos especializados en la solución integral de proyectos e infraestructura para el ciclismo urbano.</p>
-            <p>Nuestro equipo humano está conformado por ciudadanos que viven la ciudad y se transportan principalmente en bicicleta, eso nos da la sensibilidad para entender los proyectos desde la calle y no desde el escritorio.</p>
+            <?php echo $page->txt2; ?>
+            <?php echo $page->txt3; ?>
           </div>
         </div>
       </div>
@@ -20,16 +19,16 @@
     <section class="bkt-features j-bg-gray-regular">
       <div class="bkt-features-wrap">
         <div class="bkt-features-wrap-middle-down-text">
-         <h2>Bikla<sup>r</sup></h2>
+         <h2><?php echo $page->title1; ?><sup>r</sup></h2>
          <div class="text">
            <div class="subtitle-container">
              <p class="sub-title">
-               BIKLA<sup>R</sup> ha venido transformando la ciudad <strong>desde hace muchos años</strong>.
+               <?php echo str_replace("<p>", "", $page->txt4); ?>
              </p>
              <hr class="right">
            </div>
-           <p>En 2006 en la ciudad de Guadalajara BKT bici pública emprende un sistema de bicicletas públicas único en su tipo y precursor de los </p>
-           <p>sistemas de bicicletas públicas como política pública en el territorio Mexicano.</p>
+            <?php echo $page->txt5; ?>
+            <?php echo $page->txt6; ?>
          </div>
         </div>
       </div>
@@ -38,11 +37,7 @@
     <section class="bkt-features j-bg-color-warning">
       <div class="bkt-features-wrap">
         <div class="bkt-features-wrap-text fifthy-percent">
-          <p>
-           Sistema semi automatizado basado en los mismos principios de  los sistemas de bicicleta pública modernos: gestionado por software especializado, acceso a usuarios por medio de registro en línea y pago con tarjetas y efectivo con la posibilidad de realizar viajes en una sola dirección, es decir de un punto A a un punto B; dispositivo de acceso (tarjeta BiklaR), App móvil, asistido por personal de comercios o instituciones colaboradoras.
-           <br>
-           <strong>El programa estuvo vigente hasta el año 2012, con una red de más de 40 estaciones y 150 bicicletas, 5,000 usuarios aproximadamente</strong>
-          </p>
+          <?php echo $page->txt7; ?>
         </div>
         <?php $image=$page->img1;
         if($image)
@@ -54,11 +49,7 @@
     <!--  BKT Mensaje Jumbo-->
     <section class="bkt-jumbo-message">
       <div class="j-wrap">
-        <p>
-          Un sistema de bicicletas públicas cambia las reglas
-          de juego en la ciudad: donde es implementado,
-          <strong>¡La gente lo adopta espontáneamente!</strong>
-        </p>
+        <?php echo $page->txt8; ?>
         <hr>
       </div>
     </section>
@@ -67,15 +58,14 @@
       <div class="bkt-features-wrap">
         <div class="bkt-features-wrap-text fifthy-percent">
           <h2 class="j-color-inverse">
-            MIBICI Guadalajara
+            <?php echo $page->title2; ?>
             <br>
             <span class="title-lighter">
-              Guadalajara, México (Noviembre 2014)
+              <?php echo $page->title3; ?>
             </span>
           </h2>
           <hr class="right">
-          <p class="j-color-inverse">En Noviembre de 2014 en la ciudad de Guadalajara, México iniciamos la operación del sistema MIBICI. Red de 116 estaciones y 1260 bicicletas en su primer fase más 120 estaciones para la segunda fase, llegando a 236 estaciones y más de 2 mil bicicletas.</p>
-          <p class="j-color-inverse">Aportamos nuestra experiencia desde la etapa de implementación, integración de tecnología en conjunto con las instituciones financieras y gobierno, ensambles y puesta a punto del equipamiento y finalmente la instalación en calle. </p>
+          <?php echo str_replace('<p>', '<p class="j-color-inverse">', $page->txt10); ?>
         </div>
         <?php $image=$page->img2;
         if($image)
@@ -94,10 +84,10 @@
        <div class="bkt-features-wrap-photo desktop" <?php if($image) { ?> style="background-image: url('<?php echo $img_thumb->url; ?>'); <?php } ?>">
         </div>
         <div class="bkt-features-wrap-text fifthy-percent">
-          <h2>MIBICI Guadalajara</h2>
+          <h2><?php echo $page->title4; ?></h2>
+          <?php echo $page->title5; ?>
           <hr class="right">
-          <p>A partir del 1 de noviembre de 2014 somos responsables del mantenimiento de estaciones y bicicletas, nivelación y re-distribución de bicicletas en estaciones, atención a usuarios y comunicación del programa.</p>
-          <p>Creemos que la experiencia del usuario debe ser la mejor, de esta forma MIBICI prevalecerá como una verdadera opción de transporte, aportando beneficios económicos, sociales y ambientales a la ciudad y sus habitantes.</p>
+          <?php echo $page->txt11; ?>
         </div>
         <!--  The next container show a photo in mobile and hidden in desktop for esthetic reason  -->
         <?php $image=$page->img3;
@@ -110,10 +100,7 @@
    <!--  BKT Mensaje Jumbo-->
    <section class="bkt-jumbo-message">
      <div class="j-wrap">
-       <p>
-         <strong>Integrar la bicicleta</strong> como modo de transporte <br>
-        cotidiano en la población
-       </p>
+        <?php echo $page->txt12; ?>
        <hr>
      </div>
    </section>
@@ -128,15 +115,14 @@
         </div>
         <div class="bkt-features-wrap-text fifthy-percent">
           <h2 class="j-color-inverse">
-            HUIZI Toluca
+            <?php echo $page->title6; ?>
             <br>
             <span class="title-lighter">
-              Toluca de Lerdo, <br>
-              Edo. de México Diciembre 2015
+              <?php echo $page->title7; ?>
             </span>
           </h2>
           <hr class="right">
-          <p class="j-color-inverse">Nuestra participación en la ciudad de Toluca ha sido indispensable para el sistema Huizi, sobre todo por la particularidad de su esquema, donde la administración local se ha responsabilizado completamente de la operación del sistema con sus propios recursos. </p>
+          <?php echo str_replace('<p>', '<p class="j-color-inverse">', $page->txt13); ?>
         </div>
         <!--  The next container show a photo in mobile and hidden in desktop for esthetic reason  -->
         <?php $image=$page->img4;
@@ -150,11 +136,10 @@
    <section class="bkt-features j-bg-gray-lighter">
       <div class="bkt-features-wrap">
         <div class="bkt-features-wrap-text fifthy-percent">
-          <h2>HUIZI Toluca</h2>
+          <h2><?php echo $page->title8; ?></h2>
+          <?php echo $page->title9; ?>
           <hr class="right">
-          <p>
-            BKT bici pública tuvimos la responsabilidad de acompañar al equipo de gobierno con asesoría y supervisión desde la recepción de equipamiento, puesta a punto e instalación en calle. Aportamos nuestra experiencia para agilizar los procesos, garantizar la calidad del equipamiento y alcanzar las metas establecidas.
-          </p>
+          <?php echo $page->txt14; ?>
         </div>
         <?php $image=$page->img5;
         if($image)
