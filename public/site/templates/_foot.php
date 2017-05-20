a00893b75f6b3b4e7d97c52965b43b1b3afbf1b0
Jesa Navarro <jesaa@me.com>
2017-05-20 10:56:00 -0500

    Add maps api

--- public/site/templates/_foot.php
+++ public/site/templates/_foot.php
@@ -55,6 +55,7 @@
     </div>
   </footer>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
   <script>
     function toggleHeaderResponsive() {
       $('header').toggleClass("mobile-active")
