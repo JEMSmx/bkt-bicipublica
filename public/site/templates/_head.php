89c734fb7ab589c3793935b44eb742d29ba70059
Jesa Navarro <jesaa@me.com>
2017-07-14 23:39:18 -0500

    Add microdata

--- public/site/templates/_head.php
+++ public/site/templates/_head.php
@@ -4,47 +4,51 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>BKT Bicipublica | <?php echo $page->title; ?></title>
+  <title>BKT Bici pública | <?php echo $page->title; ?></title>
   <meta name="description" content="<?php echo $page->summary; ?>">
   <meta name="distribution" content="global">
   <meta name="robots" content="INDEX,FOLLOW"/>
-  <meta property="og:url"                content="http://www.bktbicipublica.com" />
+  <meta property="og:url"                content="http://www.bktbicipublica.com/" />
   <meta property="og:title"              content="BKT Bicipublica | <?php echo $page->title; ?>" />
   <meta property="og:description"        content="<?php echo $page->summary; ?>" />
-  <link rel="icon" type="image/png" href="<?php echo $config->urls->templates; ?>static/455375-1495563394/images/favicon-64x64.png" sizes="64x64">
-  <link rel="icon" type="image/png" href="<?php echo $config->urls->templates; ?>static/455375-1495563394/images/favicon-180x180.png" sizes="128x128">
+  <link rel="canonical" href="http://www.bktbicipublica.com/" />
+  <link rel="icon" type="image/png" href="<?php echo $config->urls->templates; ?>static/455375-1498674772/images/favicon-64x64.png" sizes="64x64">
+  <link rel="icon" type="image/png" href="<?php echo $config->urls->templates; ?>static/455375-1498674772/images/favicon-180x180.png" sizes="128x128">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" integrity="sha256-K3Njjl2oe0gjRteXwX01fQD5fkk9JFFBdUHy/h38ggY=" crossorigin="anonymous" rel="stylesheet">
   <link href="https://cdn.rawgit.com/cobyism/gridism/0.2.2/gridism.css" rel="stylesheet">
   <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
   <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
-  <link href="<?php echo $config->urls->templates; ?>static/455375-1495563394/styles/main.css" rel="stylesheet">
+  <link href="<?php echo $config->urls->templates; ?>static/455375-1498674772/styles/main.css" rel="stylesheet">
+  <script type="application/ld+json">{"@context":"http://schema.org","@id":"http://www.bktbicipublica.com/#organization","@type":"Organization","name":"BKT Bici pública","url":"http://www.bktbicipublica.com/","logo":"http://bktbicipublica.com/site/templates/static/455375-1498674772/images/bkt-logotipo.svg","sameAs":["https://www.facebook.com/bktbp/","https://twitter.com/BKTbicipublica"]}</script>
+<link rel='dns-prefetch' href='//cdn.rawgit.com' />
+<link rel='dns-prefetch' href='//cdnjs.cloudflare.com' />
+<link rel='dns-prefetch' href='//google-analytics.com' />
+<link rel='dns-prefetch' href='//ajax.googleapis.com' />
 </head>
-
 <body>
-
  <!--  BKT Header-->
   <header id="header1">
-    <a href="/"><img src="<?php echo $config->urls->templates; ?>static/455375-1495563394/images/bkt-logotipo.svg" alt="Logotipo BKT bici pública"></a>
-    <a id="close-icon" href="javascript:void(0);"><img src="<?php echo $config->urls->templates; ?>static/455375-1495563394/images/close-icon.svg"></a>
+    <a href="/"><img src="<?php echo $config->urls->templates; ?>static/455375-1498674772/images/bkt-logotipo.svg" alt="Logotipo BKT bici pública"></a>
+    <a id="close-icon" href="javascript:void(0);"><img src="<?php echo $config->urls->templates; ?>static/455375-1498674772/images/close-icon.svg"></a>
     <nav>
-      <ul>
+      <ul itemscope itemtype="http://www.schema.org/SiteNavigationElement">
         <a href="" title="Inicio">
           <li></li>
         </a>
-        <a href="/servicios" title="Servicios">
-          <li>Servicios</li>
+        <a itemprop="url" href="/servicios" title="Servicios">
+          <li itemprop="name">Servicios</li>
         </a>
-        <a href="/tecnologia" title="Tecnología">
-          <li>Tecnología</li>
+        <a itemprop="url" href="/tecnologia" title="Tecnología">
+          <li itemprop="name">Tecnología</li>
         </a>
-        <a href="/experiencia" title="Experiencia">
-          <li>Experiencia</li>
+        <a itemprop="url" href="/experiencia" title="Experiencia">
+          <li itemprop="name">Experiencia</li>
         </a>
-        <a href="/contacto" title="Contacto">
-          <li>Contacto</li>
+        <a itemprop="url" href="/contacto" title="Contacto">
+          <li itemprop="name">Contacto</li>
         </a>
       </ul>
     </nav>
-    <img id="menu-icon" src="<?php echo $config->urls->templates; ?>static/455375-1495563394/images/menu-icon.svg" alt="Menu icon for BKTbp">
-    <img class="bktbp-logo" src="<?php echo $config->urls->templates; ?>static/455375-1495563394/images/bkt-logo.svg" alt="BKT bici publica logo">
+    <img id="menu-icon" src="<?php echo $config->urls->templates; ?>static/455375-1498674772/images/menu-icon.svg" alt="BKT bici pública">
+    <img class="bktbp-logo" src="<?php echo $config->urls->templates; ?>static/455375-1498674772/images/bkt-logo.svg" alt="BKT bici pública logo">
   </header>
